a1e4b892ea3879d9c5bbdbfc02cac82b3c4e65eb
Selling-surprises <163849232+Selling-surprises@users.noreply.github.com>
2026-05-23 17:42:59 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'icon-workshop-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './img/favicon-128.png',
+  './img/favicon-192.png',
+  './img/favicon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error('Cache addAll error:', err))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          networkResponse => {
+            if (event.request.url.indexOf(self.location.origin) === 0 &&
+                networkResponse &&
+                networkResponse.status === 200 &&
+                event.request.method === 'GET') {
+              const responseToCache = networkResponse.clone();
+              caches.open(CACHE_NAME).then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            }
+            return networkResponse;
+          }
+        );
+      })
+  );
+});
